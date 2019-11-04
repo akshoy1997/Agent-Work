93055077f7298f01e796a0f10f72ef0cff275f22
Akshay Deuri <akshaydeuri@Akshays-MacBook-Air.local>
2019-11-04 22:30:37 +0530

    Added Password Reset Function

--- agent_work/settings.py
+++ agent_work/settings.py
@@ -143,6 +143,13 @@ LOGIN_REDIRECT_URL = 'home'
 
 LOGIN_REDIRECT_URL = 'index'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "vendor.app.django@gmail.com"
+EMAIL_HOST_PASSWORD = "xbmbdsztdxiznhal"
+
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('',views.indexView, name='index'),
+    path('', views.indexView, name='index'),
     path('home/',views.homeView, name='home'),
     path('home/meetings/',views.meetingsView, name='meetings'),
     path('lead/create/',views.leadCreateView, name='lead_create'),
@@ -15,7 +15,11 @@ urlpatterns = [
     path('converted/',views.convertedView, name='converted'),
     path('closed/',views.closedView, name='closed'),
     path('register/',views.registerView, name='register'),
-    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/',auth_views.LoginView.as_view(redirect_authenticated_user=True, template_name='users/login.html'), name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('password_reset/',auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password_reset/confirm/<uidb64>/<token>',auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password_reset/complete/',auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
     url(r'^api/', include('users.api.urls')),
 ]
