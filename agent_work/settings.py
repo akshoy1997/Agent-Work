0bd7e214d04f2ba47055e86b3c7c4ac9bf363346
Akshay Deuri <akshaydeuri@Akshays-MacBook-Air.local>
2019-11-14 01:56:13 +0530

    Secured HTTP Traffic

--- agent_work/settings.py
+++ agent_work/settings.py
@@ -121,9 +121,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-#Redirecting HTTP Traffic to HTTPS
-SECURE_SSL_REDIRECT = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
